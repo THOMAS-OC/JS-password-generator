414e88e13996f94a263ba748ee84fd917e9c9bce
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-22 15:53:42 +0200

    Création d'un objet property en vu de générer un password

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
 const resultPassword = document.querySelector('.result-password')
 
 const inputRange = document.querySelector('input[name="len"]')
@@ -8,7 +10,42 @@ const inputMaj = document.querySelector('input[name="maj"]')
 const inputNumber = document.querySelector('input[name="number"]')
 const inputSpecial = document.querySelector('input[name="special"]')
 
+const button = document.querySelector("button")
+
+// Property password
+let passwordProperty = {
+    len : 0,
+    ifMaj : false,
+    ifMin : false,
+    ifNumber : false,
+    ifSpecial : false
+}
 
 inputRange.addEventListener("change", () => {
     labelLen.innerText = inputRange.value
+    passwordProperty.len = inputRange.value
+})
+
+
+inputMaj.addEventListener("change", () => {
+    passwordProperty.ifMaj = ifMaj.checked;
+})
+
+inputMin.addEventListener("change", () => {
+    passwordProperty.ifMin = ifMin.checked;
+})
+
+inputMin.addEventListener("change", () => {
+    passwordProperty.ifNumber = ifNumber.checed;
+})
+
+inputMin.addEventListener("change", () => {
+    passwordProperty.ifSpecial = ifSpecial.cheked;
+})
+// END property password
+
+
+
+button.addEventListener("click", () => {
+
 })
