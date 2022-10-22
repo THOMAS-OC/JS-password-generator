e423c358cce5e52c6ad71cb2fb30e1c4e3ad57a6
ESTIVAL Thomas <thomasestivaloc@gmail.com>
2022-10-22 16:21:01 +0200

    Génération du mot de passe en cours

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+const specials = ["$", "-", "*", "%", "&", "!", "?", ".", ";"]
+const numbers = ["1","2","3","4","5","6","7","8","9"]
+
+let passwordGenerate = []
+
 
 const resultPassword = document.querySelector('.result-password')
-
 const inputRange = document.querySelector('input[name="len"]')
 const labelLen = document.querySelector('.label-len span')
 
-const inputMin = document.querySelector('input[name="min"]')
 const inputMaj = document.querySelector('input[name="maj"]')
 const inputNumber = document.querySelector('input[name="number"]')
 const inputSpecial = document.querySelector('input[name="special"]')
@@ -14,9 +17,8 @@ const button = document.querySelector("button")
 
 // Property password
 let passwordProperty = {
-    len : 0,
+    len : 8,
     ifMaj : false,
-    ifMin : false,
     ifNumber : false,
     ifSpecial : false
 }
@@ -26,21 +28,16 @@ inputRange.addEventListener("change", () => {
     passwordProperty.len = inputRange.value
 })
 
-
 inputMaj.addEventListener("change", () => {
-    passwordProperty.ifMaj = ifMaj.checked;
+    passwordProperty.ifMaj = inputMaj.checked;
 })
 
-inputMin.addEventListener("change", () => {
-    passwordProperty.ifMin = ifMin.checked;
+inputNumber.addEventListener("change", () => {
+    passwordProperty.ifNumber = inputNumber.checked;
 })
 
-inputMin.addEventListener("change", () => {
-    passwordProperty.ifNumber = ifNumber.checed;
-})
-
-inputMin.addEventListener("change", () => {
-    passwordProperty.ifSpecial = ifSpecial.cheked;
+inputSpecial.addEventListener("change", () => {
+    passwordProperty.ifSpecial = inputSpecial.checked;
 })
 // END property password
 
@@ -48,4 +45,28 @@ inputMin.addEventListener("change", () => {
 
 button.addEventListener("click", () => {
 
+    alert(passwordProperty.ifSpecial)
+
+    passwordGenerate = []
+
+    while (passwordGenerate.length < passwordProperty.len) {
+
+        if (passwordProperty.ifMaj) {
+            passwordGenerate.push(letters[Math.floor(Math.random() * 26)].toUpperCase())
+        }
+
+        if(passwordProperty.ifNumber) {
+            passwordGenerate.push(numbers[Math.floor(Math.random() * 10)])
+        }
+
+        if(passwordProperty.ifSpecial) {
+            passwordGenerate.push(specials[Math.floor(Math.random() * 10)])
+        }
+
+        passwordGenerate.push(letters[Math.floor(Math.random() * 26)])
+
+    }
+
+    alert(passwordGenerate);
+    
 })
